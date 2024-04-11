f73d7f24208c4fcc166ce98140b791dea538ed53
frensing <dev@frensing.de>
2024-04-11 21:54:00 +0300

    Backstage

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -27,30 +27,33 @@ export class Shop {
       }
 
       if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+        item.sellIn -= 1
+
         if (item.quality < 50) {
           item.quality += 1
 
-          if (item.sellIn < 11) {
+          if (item.sellIn < 10) {
             if (item.quality < 50) {
               item.quality += 1;
             }
-          }
-          if (item.sellIn < 6) {
-            if (item.quality < 50) {
+
+            if (item.sellIn < 5 && item.quality < 50) {
               item.quality = item.quality + 1;
-            }
+            } 
           }
         }
-
-        item.sellIn -= 1
 
         if (item.sellIn < 0) {
           item.quality -= item.quality
         }
       }
 
+      if (item.name == 'Sulfuras, Hand of Ragnaros') {
+        
+      }
 
-      
+
+
 
       if (item.name != 'Aged Brie' && item.name && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
 
